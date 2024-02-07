42da5eb1276a6cddb7d25feb3a97a3522d6807c2
Erick Gaona <Erickgaona625@gmail.com>
2024-02-07 14:30:21 -0500

    Agregando funciones y graficos al proyecto

---
+++ Proyecto-Func-Graf/src/main/scala/Funciones.scala
@@ -0,0 +1,116 @@
+import com.github.tototoshi.csv.*
+
+import java.io.File
+import scala.io.Source
+import org.nspl._
+import org.nspl.awtrenderer._
+import org.nspl.data.HistogramData
+
+implicit object CustomFormat extends DefaultCSVFormat {
+  override val delimiter: Char = ';'
+}
+
+object Funciones {
+  @main
+  def work() = {
+
+    val path2DataFilePartidosYGoles: String = "C:\\Users\\Erick Gaona\\Desktop/\\proyecto\\dsPartidosYGoles.csv\\"
+    val path2DataFileAlineacionesXTorneo: String = "C:\\Users\\Erick Gaona\\Desktop\\proyecto\\dsAlineacionesXTorneo.csv\\"
+
+    val readerPartidosYGoles = CSVReader.open(new File(path2DataFilePartidosYGoles))
+    val readerAlineacionesXTorneo = CSVReader.open(new File(path2DataFileAlineacionesXTorneo))
+    //val contentFile: List[List[String]] = reader.all()
+    val contentFilePartidosYGoles: List[Map[String, String]] = readerPartidosYGoles.allWithHeaders()
+    val contentAlineacionesXTorneo: List[Map[String, String]] = readerAlineacionesXTorneo.allWithHeaders()
+
+    readerPartidosYGoles.close()
+    readerAlineacionesXTorneo.close()
+
+
+    // La capacidad mínima, máxima y el promedio de la capacidad de los estadios en los que se ha jugado algún partido del mundial. La columna que deben utilizar es: stadiums_stadium_capacity.
+
+    // Extraer la capacidad de los estadios y calcular la capacidad mínima, máxima y promedio
+    val capacidad: List[Int] = contentFilePartidosYGoles
+      .flatMap(_.get("stadiums_stadium_capacity").filter(_.forall(_.isDigit)).map(_.toInt))
+    val minCapacidad = capacidad.min
+    val maxCapacidad = capacidad.max
+    val promCapacidad = capacidad.sum / capacidad.length
+
+    println(s"Capacidad mínima: $minCapacidad")
+    println(s"Capacidad máxima: $maxCapacidad")
+    println(s"Capacidad promedio: $promCapacidad")
+
+    // ¿Cuál es el minuto más común en el que se han marcado un gol? Su respuesta debe presentar el resultado para los
+    // torneos masculinos y otra los torneos femeninos.
+    //Hombres
+
+    val minutoMasComunMasculino= contentFilePartidosYGoles
+      .filter(x => x("tournaments_tournament_name").contains("Men"))
+      .filter(_("goals_minute_regulation") != "NA")
+      .map(x => x("goals_minute_regulation"))
+      .groupBy(identity)
+      .map(x => x._1 -> x._2.length)
+      .maxBy(_._2)._1
+
+    //Mujeres
+
+    val minutoMasComunFemenino = contentFilePartidosYGoles
+      .filter(x => x("tournaments_tournament_name").contains("Women"))
+      .filter(_("goals_minute_regulation") != "NA")
+      .map(x => x("goals_minute_regulation"))
+      .groupBy(identity)
+      .map(x => x._1 -> x._2.length)
+      .maxBy(_._2)._1
+
+
+
+    println(s"Minuto más común en torneos masculinos: $minutoMasComunMasculino")
+    println(s"Minuto más común en torneos femeninos: $minutoMasComunFemenino")
+
+    //¿Cuál es el periodo más común en los que se han marcado goles en todos los mundiales? (columna: goals_match_period)
+
+    // Obtener todos los minutos de gol
+    val minutos = contentFilePartidosYGoles.flatMap(_("goals_match_period").split(","))
+
+    // Encontrar el periodo más común
+    val periodo = minutos.groupBy(identity).maxBy(_._2.length)._1
+
+
+    // Imprimir resultado
+    println(s"Periodo más común en el que se han marcado goles en todos los mundiales: $periodo")
+
+
+    // ¿Cuál es el número de camiseta (squads_shirt_number) más común que se utiliza en cada una de las posiciones
+    // (squads_position_name)
+
+    // Agrupar por posición y número de camiseta y contar la frecuencia
+    val posicionYnumero = contentAlineacionesXTorneo.groupBy(record => (record("squads_position_name"), record("squads_shirt_number")))
+      .mapValues(_.size)
+
+    // Encontrar el número de camiseta más común para cada posición
+    val numerocamisetaComun = posicionYnumero.groupBy(_._1._1)
+      .mapValues(_.maxBy(_._2)._1._2)
+
+    numerocamisetaComun.foreach((position, shirtNumber) => println(s"Posición: $position, Número de camiseta más común: $shirtNumber"))
+
+    // Encontrar la frecuencia de los marcadores en los mundiales de fútbol
+    val frecuenciaMarcadores = contentFilePartidosYGoles
+      .flatMap(x => List(x("matches_home_team_score"),x("matches_away_team_score")))
+      .groupBy(identity)
+      .mapValues(_.size)
+
+    // Imprimir la frecuencia de los marcadores
+    println("Frecuencia de los marcadores:")
+    frecuenciaMarcadores.foreach { case (marcador, frecuencia) =>
+      println(s"$marcador: $frecuencia")
+
+
+
+
+    }
+
+  }
+
+
+
+}
---
+++ Proyecto-Func-Graf/src/main/scala/Graficas.scala
@@ -0,0 +1,189 @@
+import cats.effect.IO
+import com.github.tototoshi.csv.*
+import org.nspl.*
+import org.nspl.awtrenderer.*
+import org.saddle.*
+import cats.*
+import doobie.*
+import doobie.implicits.*
+import java.io.File
+import cats.effect.unsafe.implicits.global
+import doobie.Transactor
+import breeze.plot._
+
+object Graficas {
+
+  @main
+  def integral2() =
+    val path2DataFilePartidosYGoles: String = "C:\\Users\\Erick Gaona\\Desktop/\\proyecto\\dsPartidosYGoles.csv\\"
+    val path2DataFile: String = "C:\\Users\\Erick Gaona\\Desktop/\\proyecto\\dsAlineacionesXTorneo.csv\\"
+    val reader = CSVReader.open(new File(path2DataFile))
+    val reader2 = CSVReader.open(new File(path2DataFilePartidosYGoles))
+
+    val contentFile: List[Map[String, String]] = reader.allWithHeaders()
+    val contentFile2:List[Map[String, String]] = reader2.allWithHeaders()
+    reader.close()
+    reader2.close()
+
+    //Conexion BD
+    val xa = Transactor.fromDriverManager[IO](
+      driver = "com.mysql.cj.jdbc.Driver",
+      url = "jdbc:mysql://localhost:3306/practicum",
+      user = "root",
+      password = "root123",
+      logHandler = None
+    )
+    //Metodos graficos desde el CSV
+
+    densityNumeroC(contentFile)
+    frecuenciaGoles(contentFile2)
+    ganadoresdeMundiales(contentFile2)
+
+    //Metodo graficos desde BD
+    CapacidadMaxE(EstadiosCapMgrafico().transact(xa).unsafeRunSync())
+    graficaEdadGoles(edadGoles().transact(xa).unsafeRunSync())
+    graficaEdadPartidos((edadPartidos()).transact(xa).unsafeRunSync())
+
+    //CSV
+
+    // numero de camiseta de los delanteros
+    def densityNumeroC(data: List[Map[String, String]]): Unit = {
+      val listNroShirt: List[Double] = data
+        .filter(row => row("squads_position_name") == "forward" && row("squads_shirt_number") != "0")
+        .map(row => row("squads_shirt_number").toDouble)
+
+      val densityforwardNumber = xyplot(density(listNroShirt.toVec.toSeq) -> line())(
+        par
+          .xlab("Numero de camiseta") // eje x
+          .ylab("freq.") // eje y
+          .main("Forward Shirt Number") // grafica nombre
+      )
+
+      pngToFile(new File("C:\\Users\\Erick Gaona\\Desktop\\Proyecto Bimestral\\Graficas\\CSV_NumeroCamisetaDelanteros.png"), densityforwardNumber.build, 1000)
+
+    }
+    //Frecuencia de goles de cada mundial
+    def frecuenciaGoles(data: List[Map[String,String]]): Unit = {
+      val frecuenci = data
+        .map(x => (x("matches_home_team_score"),x("matches_away_team_score")))
+        .groupBy(_._2)
+        .map(k=>(k._1,k._2.size.toDouble))
+
+
+      val densitFrecuencia = xyplot(density(frecuenci.toSeq.map(_._2).toIndexedSeq) -> line())(
+
+
+        par
+          .xlab("Numero de Goles")
+          .ylab("Frecuncia")
+          .main("Frecuencia de goles Mundial")
+
+      )
+      pngToFile(new File("C:\\Users\\Erick Gaona\\Desktop\\Proyecto Bimestral\\Graficas\\CSV_FrecuenciaGoles.png"), densitFrecuencia.build, 1000)
+
+    }
+    //Cuantos mundiales a ganado cada pais
+    def ganadoresdeMundiales(data: List[Map[String, String]]) =
+      val winners = data
+        .map(row => (row("matches_tournament_id"), row("tournaments_winner")))
+        .distinct
+        .groupBy(_._2)
+        .map(row => (row._1, row._2.size.toDouble))
+
+      val ganadoresIndices = Index(winners.map(r => r._1).toArray)
+      val valores = Vec(winners.map(valores => valores._2).toArray)
+
+      val series = Series(ganadoresIndices, valores)
+      val bar1 = saddle.barplotHorizontal(series,
+        xLabFontSize = Option(RelFontSize(2)),
+        color = RedBlue(80, 171))(
+        par
+          .xLabelRotation(-77)
+          .xNumTicks(0)
+          .xlab("Torneos")
+          .ylab("Cantidad de mundiales")
+          .main("Paises")
+      )
+      pngToFile(new File("C:\\Users\\Erick Gaona\\Desktop\\Proyecto Bimestral\\Graficas\\CSV_FrecGanadores.png"), bar1.build, 1000)
+
+  //BD
+  // Mostrar los estadios y su capacidad en un país específico:
+  def EstadiosCapMgrafico():ConnectionIO[List[(String, Double)]] = {
+    sql"""
+          SELECT s.name as StadiumName, s.capacity as Capacity
+    FROM stadium s
+    INNER JOIN country c ON s.countryId = c.countryId
+    WHERE c.countryName = 'Argentina';
+       """
+      .query[(String, Double)]
+      .to[List]
+  }
+  def CapacidadMaxE(data: List[(String, Double)]) = {
+    val indices = Index(data.map(_._1).toArray)
+    val values = Vec(data.map(_._2).toArray)
+
+    val series = Series(indices, values)
+
+    val barPlot = saddle.barplotHorizontal(series,
+      xLabFontSize = Option(RelFontSize(0.5))
+    )(
+      par
+        .xLabelRotation(-90)
+        .xNumTicks(0)
+        .xlab("Estadio")
+        .ylab("Capacidad")
+        .main("Capacidad Maxima")
+    )
+
+    pngToFile(new File("C:\\Users\\Erick Gaona\\Desktop\\Proyecto Bimestral\\Graficas\\BD_EstadiosCap.png"),barPlot.build,5000)
+  }
+
+  // Gráfica de dispersión sobre la relación entre la edad de los jugadores (desde 1940) y el número de goles que han marcado en los partidos.
+  def edadGoles(): ConnectionIO[List[(Int, Int)]] = {
+    sql"""
+      SELECT (YEAR(CURRENT_DATE) - YEAR(birthDay)) AS edad, COUNT(goalId) AS total_goles
+          FROM player
+          INNER JOIN squad ON player.playerid = squad.playerid
+          INNER JOIN goal ON squad.playerid = goal.playerId
+          WHERE YEAR(birthDay) >= 1940
+          GROUP BY edad;
+    """
+      .query[(Int, Int)]
+      .to[List]
+  }
+  def graficaEdadGoles(data: List[(Int, Int)]): Unit = {
+    val f = Figure()
+    val p = f.subplot(0)
+    p += plot(data.map(_._1), data.map(_._2), '+' )
+    p.xlabel = "Edad"
+    p.ylabel = "Goles"
+    f.saveas("C:\\Users\\Erick Gaona\\Desktop\\Proyecto Bimestral\\Graficas\\BD_PartidosEGoles.png")
+
+  }
+
+  // Gráfica de dispersión sobre la relación entre la edad de los jugadores (desde 1930) y la cantidad de partidos en los que han participado
+  def edadPartidos(): ConnectionIO[List[(Int, Int)]] = {
+    sql"""
+      SELECT (YEAR(CURRENT_DATE) - YEAR(birthDay)) AS edad, COUNT(matchs.matchId) AS total_partidos
+          FROM player
+          INNER JOIN squad ON player.playerid = squad.playerid
+          INNER JOIN matchs ON squad.teamId = matchs.homeTeamId OR squad.teamId = matchs.AwayTeamId
+          WHERE YEAR(birthDay) >= 1930
+          GROUP BY edad;
+    """
+      .query[(Int, Int)]
+      .to[List]
+  }
+  def graficaEdadPartidos(data: List[(Int, Int)]): Unit = {
+    val f = Figure()
+    val p = f.subplot(0)
+    p += plot(data.map(_._1), data.map(_._2), '+')
+    p.xlabel = "Edad"
+    p.ylabel = "Partidos Participados"
+    f.saveas("C:\\Users\\Erick Gaona\\Desktop\\Proyecto Bimestral\\Graficas\\BD_PartidosECantidad.png")
+
+
+  }
+
+}
+
