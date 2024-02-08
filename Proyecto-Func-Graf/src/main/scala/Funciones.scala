891732b4d0177a4623cf34298d2a9b3a3b77374f
Oliver Saraguro <115806636+OliverRobert33@users.noreply.github.com>
2024-02-08 10:29:27 -0500

    Funciones.scala

--- Proyecto-Func-Graf/src/main/scala/Funciones.scala
+++ Proyecto-Func-Graf/src/main/scala/Funciones.scala
@@ -29,7 +29,9 @@ object Funciones {
 
      // Extraer la capacidad de los estadios y calcular la capacidad mínima, máxima y promedio
     val capacidad: List[Int] = contentFilePartidosYGoles
-      .flatMap(_.get("stadiums_stadium_capacity").filter(_.forall(_.isDigit)).map(_.toInt))
+      .flatMap(_.get("stadiums_stadium_capacity")
+      .filter(_.forall(_.isDigit)).map(_.toInt)) //filtra los caracteres de cada capacidad para quedarse solo con los dígitos
+    
     val minCapacidad = capacidad.min
     val maxCapacidad = capacidad.max
     val promCapacidad = capacidad.sum / capacidad.length
@@ -39,19 +41,17 @@ object Funciones {
     println(s"Capacidad promedio: $promCapacidad")
 
     // ¿Cuál es el minuto más común en el que se han marcado un gol? 
-    // torneos masculinos y otra los torneos femeninos.
+    // Torneos masculinos y otra los torneos femeninos.
     //Hombres
-
     val minutoMasComunMasculino= contentFilePartidosYGoles
-      .filter(x => x("tournaments_tournament_name").contains("Men"))
-      .filter(_("goals_minute_regulation") != "NA")
-      .map(x => x("goals_minute_regulation"))
-      .groupBy(identity)
-      .map(x => x._1 -> x._2.length)
-      .maxBy(_._2)._1
+      .filter(x => x("tournaments_tournament_name").contains("Men")) //Filtra los que cumplan la clave y contienan la cadena "Men".
+      .filter(_("goals_minute_regulation") != "NA") //Filtra los sean diferentes a "NA".
+      .map(x => x("goals_minute_regulation")) // Mapeamos por la clave 
+      .groupBy(identity) // Agrupamos los minutos de los goles para contar cuántas veces ocurre cada minuto. 
+      .map(x => x._1 -> x._2.length) // Los mapeamos por clave(minutos) y valor(veces).
+      .maxBy(_._2)._1 // Encuentramos el par clave-valor que tiene el valor máximo.
 
     //Mujeres
-
     val minutoMasComunFemenino = contentFilePartidosYGoles
       .filter(x => x("tournaments_tournament_name").contains("Women"))
       .filter(_("goals_minute_regulation") != "NA")
@@ -60,32 +60,32 @@ object Funciones {
       .map(x => x._1 -> x._2.length)
       .maxBy(_._2)._1
 
-
-
     println(s"Minuto más común en torneos masculinos: $minutoMasComunMasculino")
     println(s"Minuto más común en torneos femeninos: $minutoMasComunFemenino")
-
-    //¿Cuál es el periodo más común en los que se han marcado goles en todos los mundiales? 
-
+    
+    // ¿Cuál es el periodo más común en los que se han marcado goles en todos los mundiales? 
     // Obtener todos los minutos de gol
-    val minutos = contentFilePartidosYGoles.flatMap(_("goals_match_period").split(","))
-
+    val minutos = contentFilePartidosYGoles
+    .flatMap(_("goals_match_period").split(","))
+    
     // Encontrar el periodo más común
-    val periodo = minutos.groupBy(identity).maxBy(_._2.length)._1
+    val periodo = minutos
+    .groupBy(identity)
+    .maxBy(_._2.length)._1
 
-
-    // Imprimir resultado
+    // Resultado
     println(s"Periodo más común en el que se han marcado goles en todos los mundiales: $periodo")
 
 
     // ¿Cuál es el número de camiseta  más común que se utiliza en cada una de las posiciones
-   
     // Agrupar por posición y número de camiseta y contar la frecuencia
-    val posicionYnumero = contentAlineacionesXTorneo.groupBy(record => (record("squads_position_name"), record("squads_shirt_number")))
-      .mapValues(_.size)
+    val posicionYnumero = contentAlineacionesXTorneo
+    .groupBy(record => (record("squads_position_name"), record("squads_shirt_number")))
+    .mapValues(_.size)
 
     // Encontrar el número de camiseta más común para cada posición
-    val numerocamisetaComun = posicionYnumero.groupBy(_._1._1)
+    val numerocamisetaComun = posicionYnumero
+      .groupBy(_._1._1)
       .mapValues(_.maxBy(_._2)._1._2)
 
     numerocamisetaComun.foreach((position, shirtNumber) => println(s"Posición: $position, Número de camiseta más común: $shirtNumber"))
@@ -98,12 +98,7 @@ object Funciones {
 
     // Imprimir la frecuencia de los marcadores
     println("Frecuencia de los marcadores:")
-    frecuenciaMarcadores.foreach { case (marcador, frecuencia) =>
-      println(s"$marcador: $frecuencia")
-
-
-
-
+    frecuenciaMarcadores.foreach { case (marcador, frecuencia) => println(s"$marcador: $frecuencia")
     }
 
   }
